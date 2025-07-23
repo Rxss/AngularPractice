2fd298e108c9c7b6c8c385ed594feeec3a4c0a27
Rxss <rossarmo99@gmail.com>
2025-07-23 22:19:48 +0100

    added homepage

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('angularPractice');
+  protected readonly title = signal('angular Practice');
 }
